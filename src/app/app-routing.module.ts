822fce012c39aca6f3d71d1d9ca6339daf569753
Tarique Akhtar <tarique.ansari@bluelabellabs.com>
2022-08-23 08:06:32 +0530

    added comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import {
 } from './components';
 import { LoginComponent } from './modules/shared/components/login/login.component';
 
+// routing array which contain all the routes including child route
 const routes: Routes = [
   {
     path: '',
